4a3c244314a0089342c5872df9def6e9d783c62a
tanglgt <minxia.tang@gmail.com>
2019-01-18 15:53:36 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
-git clone https://github.com/cdhigh/KindleEar.git
+git clone https://github.com/tanglgt/KindleEar.git
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
